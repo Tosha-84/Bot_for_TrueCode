46d0c8d64e96185e9d911f9bc09bb6270a907b9a
Anton Vilachev <64183970+Tosha-84@users.noreply.github.com>
2022-11-06 17:51:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -103,7 +103,7 @@ async def channel_caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('5648589910:AAFArjtVL_BcnCKzRbJcKRpnlfRJV97YLWo').build()
+    application = ApplicationBuilder().token('Здесь Токен').build()
 
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     caps_handler = CommandHandler('caps', caps)
