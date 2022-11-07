c57d12f93b8bd19e43c1036dc20b89e60fc1733c
PilotSoul <theademari@gmail.com>
2022-11-07 11:13:57 +0800

    parsing added

---
+++ excel_parsing.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+data = [['Гамаюнова Аделина', '89842740104'], ['Вилачёв Антон', '89148891191']]
+excel_file = "numbers.xlsx"
+
+
+def create_excel(data, filename):
+    df = pd.DataFrame(data, columns=['ФИО', 'Номер'])
+    df.to_excel(filename, index=False)
+
+
+def parse_excel(filename):
+    users_dict = {}
+    parsed_df = pd.read_excel(filename)
+    for index, row in parsed_df.iterrows():
+        users_dict[row['ФИО']] = row['Номер']
+    return users_dict
+
+
+users = parse_excel(excel_file)
+print(users)
