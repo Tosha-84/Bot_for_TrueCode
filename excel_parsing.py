5953bbb221387e18ddcee273bf7213e60d909099
PilotSoul <theademari@gmail.com>
2022-11-08 15:56:45 +0800

    phone number parsing added

--- excel_parsing.py
+++ excel_parsing.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import re
 
 # data = [['Гамаюнова Аделина', '89842740104'], ['Вилачёв Антон', '89148891191']]
-# excel_file = "numbers.xlsx"
+excel_file = "numbers.xlsx"
 
 
 def create_excel(data, filename):
@@ -9,14 +10,19 @@ def create_excel(data, filename):
     df.to_excel(filename, index=False)
 
 
-async def parse_excel(filename):
+def parse_number(df):
+    num_re = re.compile(r".*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*")
+    df['Номер'] = df['Номер'].apply(lambda x: "+7" + ''.join(num_re.match(str(x)).groups()))
+    return df
+
+
+def parse_excel(filename):
     users_dict = {}
-    parsed_df = pd.read_excel(filename)
+    parsed_df = parse_number(pd.read_excel(filename))
     for index, row in parsed_df.iterrows():
-        # users_dict[row['ФИО']] = row['Номер']
         users_dict[row['Номер']] = row['ФИО']
     return users_dict
 
 
-# users = parse_excel(excel_file)
-# print(users)
+users = parse_excel(excel_file)
+print(users)
