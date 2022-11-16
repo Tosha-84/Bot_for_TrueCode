31ba6efaa69b89cddd171e72217e0b8beaaeb5af
PilotSoul <theademari@gmail.com>
2022-11-16 16:18:06 +0800

    command handler added

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import telebot
-# import Human
-from telebot.types import InputMediaPhoto
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from test1 import *
+
 # API_TOKEN = '1986393023:AAGmgNDQVTn_MmBBP1Y8DMJqLVSxwvQOvV8'
 API_TOKEN = '5648589910:AAFArjtVL_BcnCKzRbJcKRpnlfRJV97YLWo'
 bot = telebot.TeleBot(API_TOKEN)
@@ -13,77 +14,79 @@ def String_to_Command(string):
     return string
 
 
-# Handle '/start'
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    # keyboard = telebot.types.ReplyKeyboardMarkup()
-    # keyboard.row('Выбрать расу')
-    # bot.reply_to(message, 'Привет, если ты здесь, значит тебе нужно Выбрать расу, так давай же начнём',
-    #              reply_markup=keyboard)
+def make_markup_wo_upload_file():
+    markup = telebot.types.InlineKeyboardMarkup()
+    buttonA = telebot.types.InlineKeyboardButton('Остановить добавление', callback_data='stop')
+    buttonC = telebot.types.InlineKeyboardButton('Начать добавление', callback_data='start')
+    buttonD = telebot.types.InlineKeyboardButton('Настройки', callback_data='settings')
 
-    bot.send_message(message.chat.id, '\U00002694    Привет! Я позволю тебе управлять добавлением пользователей в канал'
-                                      'телеграм из файла. \n'
-                                      '\U00002694    Для удобства пользования не нужно отправлять мне сообщения '
-                                      '(за исключениемм файлов), достаточно делать выбор из предложенных'
-                                      'вариантов.')
+    markup.row(buttonC, buttonA)
+    markup.row(buttonD)
 
+    return markup
+
+
+def make_markup():
     markup = telebot.types.InlineKeyboardMarkup()
     buttonA = telebot.types.InlineKeyboardButton('Остановить добавление', callback_data='stop')
     buttonB = telebot.types.InlineKeyboardButton('Загрузить документ с номерами', callback_data='upload')
     buttonC = telebot.types.InlineKeyboardButton('Начать добавление', callback_data='start')
     buttonD = telebot.types.InlineKeyboardButton('Настройки', callback_data='settings')
 
-
     markup.row(buttonB)
     markup.row(buttonC, buttonA)
     markup.row(buttonD)
+
+    return markup
+
+
+# Handle '/start'
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    """
+    :param message: class 'telebot.types.Message'
+    Обработчик команды start, выдает пользователю меню для выбора дальнейших действий
+    """
+    bot.send_message(message.chat.id, '\U00002694    Привет! Я позволю тебе управлять добавлением пользователей в канал'
+                                      'телеграм из файла. \n'
+                                      '\U00002694    Для удобства пользования не нужно отправлять мне сообщения '
+                                      '(за исключениемм файлов), достаточно делать выбор из предложенных'
+                                      'вариантов.')
+
+    markup = make_markup()
 
     bot.send_message(message.chat.id, 'Выберите необходимый пункт',
                      reply_markup=markup)
 
 
-@bot.message_handler(commands=['help'])
-def help_message(message):
-    markup = telebot.types.InlineKeyboardMarkup()
-    buttonA = telebot.types.InlineKeyboardButton('Загрузить файл', callback_data='Загрузить файл')
-
-
-    markup.row(buttonA)
-
-    bot.send_message(message.chat.id, 'Добавить файл',
-                     reply_markup=markup)
-
-
-@bot.message_handler(content_types={'text'})
-def fucking_animal(message):
-    if message.text == 'Выбрать расу':
-        markup = telebot.types.InlineKeyboardMarkup()
-        buttonA = telebot.types.InlineKeyboardButton('Человек', callback_data='Человек')
-        buttonB = telebot.types.InlineKeyboardButton('Дварф', callback_data='Дварф')
-        buttonC = telebot.types.InlineKeyboardButton('Эльф', callback_data='Эльф')
-        buttonD = telebot.types.InlineKeyboardButton('Гном', callback_data='Гном')
-        buttonE = telebot.types.InlineKeyboardButton('Полурослик', callback_data='Полурослик')
-        buttonF = telebot.types.InlineKeyboardButton('Полуорк', callback_data='Полуорк')
-        buttonG = telebot.types.InlineKeyboardButton('Полуэльф', callback_data='Полуэльф')
-        buttonH = telebot.types.InlineKeyboardButton('Драконорождённый', callback_data='Драконорождённый')
-        buttonI = telebot.types.InlineKeyboardButton('Тифлинг', callback_data='Тифлинг')
-
-        markup.row(buttonA, buttonB, buttonC, buttonD)
-        markup.row(buttonE, buttonF, buttonG, buttonI)
-        markup.row(buttonG, buttonH)
-
-        bot.send_message(message.chat.id, 'Выбери расу, прочти про неё и реши, подходит ли она для тебя',
-                         reply_markup=markup)
+@bot.message_handler(content_types=['document'])
+def get_file(message):
+    file_name = message.document.file_name
+    file_extension = file_name.split('.')[-1]
+    if file_extension != 'xlsx':
+        markup = make_markup()
+        bot.send_message(message.chat.id, 'Бот умеет обрабатывать только xlsx файлы(', reply_markup=markup)
+    else:
+        file_info = bot.get_file(message.document.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        with open(f"{message.chat.id}_file.xlsx", 'wb') as new_file:
+            new_file.write(downloaded_file)
+        markup = make_markup_wo_upload_file()
+        bot.send_message(message.chat.id, 'Файл успешно загружен', reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle(call):
     if call.data == 'start':
-        print()
+        scheduler = AsyncIOScheduler()
+        filename = f"{call.message.chat.id}_file.xlsx"
+        scheduler.add_job(job, "interval", args=[filename], seconds=60)
+        scheduler.start()
+        app.run()
     elif call.data == 'stop':
         print()
     elif call.data == 'upload':
-        print()
+        bot.send_message(chat_id=call.message.chat.id, text='Загрузите файл в формате xlsx')
     elif call.data == 'settings':
         print()
         markup = telebot.types.InlineKeyboardMarkup()
@@ -117,10 +120,6 @@ def handle(call):
                                                                                                      'необходимый'
                                                                                                      'пункт',
                               reply_markup=markup)
-
-
-
-######
 
 
 bot.polling()
--- test1.py
+++ test1.py
@@ -1,16 +1,6 @@
 import asyncio
-
-import aioschedule
-import aioschedule as schedule
-import time
-
-
 from pyrogram import client, filters
-from pyrogram.types import InputPhoneContact
-import time
-# import excel_parsing
 from excel_parsing import parse_excel
-
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 
@@ -19,80 +9,29 @@ api_hash = "9260bd2eba93540061e9aef0f518ea7e"
 app = client.Client("my_account", api_id=api_id, api_hash=api_hash)
 
 
-# Одна функция пока что, делить потом буду, когда пойму, как работает библиотека aioschedule
-# В ней берётся список подписчиков, словарь из файла, из словаря удаляются подписчики и добавляется первый
-async def job(message='stuff', n=1):
-    print("Проверка вызова")
-    # print("Asynchronous invocation (%s) of I'm working on:" % n, message)
-    # await message.reply_text(message.text)
-    # Словарь из файла
-    people = await parse_excel("numbers.xlsx")
-
-    # print(people)
-    # print(type(people))
-
-    # Цикл для удаления подписчиков из файла
+async def job(filename: str):
+    people = await parse_excel(filename)
     async for member in app.get_chat_members(-1001567792707):
-        # print(member)
         try:
-            # Сама строчка для удаления, сделано через try, потому что phone_number может быть none
             people.pop("+" + member.user.phone_number)
             print(people["+" + member.user.phone_number])
         except:
             print("Был бы ты человек")
-    # print(type(users))
-    # print(people)
-
-
-    print("Прочёсывание")
-    for index, elem in enumerate(people): # index может помочь с прочёсыванием не с нуля
-        # print(elem)
-        # print(people[elem])
-        # count = await app.get_contacts_count()
-        # print("До:", count)
-
-        # Дообавление пользователя в контакты
-        new_user = await app.import_contacts([
-            InputPhoneContact(elem, people[elem])
-        ])
-        # print(new_user.users[0].id)
-
-        # count = await app.get_contacts_count()
-        # print("После:", count)
-
-        # Добавление контакта в канал
-        try:
-            await app.add_chat_members(-1001567792707, new_user.users[0].id)
-            break
-        except:
-            continue
-
-    # Эта строчка здесь просто по аналогии из документации к aioschedule
+    # for index, elem in enumerate(people):
+    #     # Дообавление пользователя в контакты
+    #     new_user = await app.import_contacts([
+    #         InputPhoneContact(elem, people[elem])
+    #     ])
+    #     try:
+    #         await app.add_chat_members(-1001567792707, new_user.users[0].id)
+    #         break
+    #     except:
+    #         continue
     await asyncio.sleep(1)
 
 
-
-scheduler = AsyncIOScheduler()
-scheduler.add_job(job, "interval", seconds=60)
-
-scheduler.start()
-app.run()
-
-# app.run()
-
-
-
-# Это было в документации к библиотеке aioschedule
-# for i in range(1, 2):
-#     schedule.every(20).seconds.do(job, n=i)
-# schedule.every(5).to(10).days.do(job)
-# schedule.every().hour.do(job, message='things')
-# schedule.every().day.at("10:30").do(job)
-#
-# loop = asyncio.get_event_loop()
-
-
-# while True:
-#     # app.run() # Вот это где-то должно быть, но я не знаю, где, без этого ошибка о том, что бот не запущен
-#     loop.run_until_complete(schedule.run_pending())
-#     time.sleep(0.1)
+async def start_bot(filename: str):
+    scheduler = AsyncIOScheduler()
+    scheduler.add_job(job, "interval", args=[filename], seconds=60)
+    scheduler.start()
+    app.run()
